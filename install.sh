cee15a8dce945a3f88b65785b38583784356003c
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2024-12-01 15:19:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo hwclock --hctosys
 sudo apt update
 sudo apt -y upgrade
 
-sudo apt install -y curl wget git stow gpg
+sudo apt install -y curl stow gpg
 
 sudo apt autoremove -y
 
@@ -15,24 +15,6 @@ curl -LO https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 rm nvim-linux64.tar.gz
-
-sudo mkdir -p -m 755 /etc/apt/keyrings
-wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
-sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh -y
-
-KEY_FILE="$HOME/.ssh/id_ed25519"
-if [ ! -f "$KEY_FILE" ]; then
-	ssh-keygen -t ed25519 -C "27211248+rcjorgensen@users.noreply.github.com" -f "$HOME/.ssh/id_ed25519" -N ""
-	eval "$(ssh-agent -s)"
-	ssh-add ~/.ssh/id_ed25519
-fi
-
-if ! gh auth status; then
-	gh auth login
-fi
 
 NVM_DIR="$HOME/.nvm"
 if [ ! -d "$NVM_DIR" ]; then
