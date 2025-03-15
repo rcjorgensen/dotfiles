0b033d3cb2a1397aefebe5b5cf062ab1d133d9e4
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-03-15 16:47:25 +0100

    Add fzf and zoxide to package install list

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ set -xe
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y git wget curl stow unzip ripgrep build-essential zsh tmux
-
+sudo apt install -y git wget curl stow unzip ripgrep build-essential zsh tmux fzf zoxide
 sudo apt autoremove -y
 
 mkdir -p "$HOME/.config"
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,6 +3,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
 color_scheme = "Catppuccin Mocha"
 
 return config
