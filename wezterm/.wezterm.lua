b8c147db338c8ee7d498cfddbf26bf1227f5fceb
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-03-15 17:21:14 +0100

    Fix color scheme config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,6 +5,6 @@ local config = wezterm.config_builder()
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
-color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Catppuccin Mocha"
 
 return config
