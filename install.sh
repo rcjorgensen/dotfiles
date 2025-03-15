3a5a47ebadbce44cb6cc048b8fa6f74026485dd8
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-03-15 15:58:21 +0100

    Add zsh to install script

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 set -xe
 
 sudo apt update
-sudo apt -y upgrade
+sudo apt upgrade -y
 
-sudo apt install -y git wget curl stow unzip ripgrep build-essential
+sudo apt install -y git wget curl stow unzip ripgrep build-essential zsh
 
 sudo apt autoremove -y
+
+chsh -s $(which zsh)
 
 mkdir -p "$HOME/.config"
 
