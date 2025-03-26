76af0c41638471da59d08665c4ae2ed091dd28d4
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-03-26 07:56:44 +0100

    Update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,7 +3,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
 
 config.color_scheme = "Catppuccin Mocha"
 
